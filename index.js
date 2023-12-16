988598ecac1163919cffc9840792df7c4dd06a34
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-16 17:34:08 +0300

    Comleted assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
+let random = Math.ceil(Math.random() * 100);
+
+const num3 =64;
+const num4 =60;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 5, 20, 15);
+
+module.exports = {
+    multiply,
+    random,
+    mod,
+    max,
+
+};
 // Write your code here
